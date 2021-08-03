30e146f7cbdccca0eb39e7ef78c91681d3e815a4
Hanguyen2332 <thanhha2332@gmail.com>
2021-08-03 10:30:35 +0700

    bai_09_web_overview

--- _09_web_overview/practice/practice2/src/main/java/GetLoginServlet.java
+++ _09_web_overview/practice/practice2/src/main/java/GetLoginServlet.java
@@ -1,10 +1,29 @@
 import java.io.IOException;
 import java.io.PrintWriter;
 
-@javax.servlet.annotation.WebServlet(name = "LoginServlet", urlPatterns = "/login")
+@javax.servlet.annotation.WebServlet(name = "LoginServlet", urlPatterns = "/post_servlet")
 public class GetLoginServlet extends javax.servlet.http.HttpServlet {
     protected void doPost(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
-
+//        String username = request.getParameter("username");
+//        String password = request.getParameter("password");
+//        PrintWriter writer = response.getWriter();
+//        if (username.equals("admin") && password.equals("admin")) {
+//            writer.println("<html>");
+//            writer.println("<body>");
+//            writer.println("<p style='text-align:center; color:blue;font-size: 40px'>");
+//            writer.println("Welcome admin to website!");
+//            writer.println("</p>");
+//            writer.println("</body>");
+//            writer.println("</html>");
+//        } else {
+//            writer.println("<html>");
+//            writer.println("<body>");
+//            writer.println("<p style='text-align:center; color:red; font-size: 40px'>");
+//            writer.println("Login Error!");
+//            writer.println("</p>");
+//            writer.println("</body>");
+//            writer.println("</html>");
+//        }
     }
 
     protected void doGet(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
--- _09_web_overview/practice/practice2/src/main/webapp/index.jsp
+++ _09_web_overview/practice/practice2/src/main/webapp/index.jsp
@@ -13,20 +13,20 @@
 <body>
 <%--GET--%>
 
-<%--<form action="/login" method="get">--%>
-<%--    <h1>Login</h1>--%>
-<%--    <input type="text" name="username" placeholder="username" style="height: 20px"><br><br>--%>
-<%--    <input type="text" name="password" placeholder="password" style="height: 20px"><br><BR>--%>
-<%--    <input type="submit" value="Sign in">--%>
-<%--</form>--%>
-
-<%--POST--%>
-<form action="/post_servlet.jsp" method="post">
+<form action="/login" method="get">
     <h1>Login</h1>
     <input type="text" name="username" placeholder="username" style="height: 20px"><br><br>
     <input type="text" name="password" placeholder="password" style="height: 20px"><br><BR>
     <input type="submit" value="Sign in">
 </form>
+
+<%--POST--%>
+<%--<form action="/post_servlet.jsp" method="post">--%>
+<%--    <h1>Login</h1>--%>
+<%--    <input type="text" name="username" placeholder="username" style="height: 20px"><br><br>--%>
+<%--    <input type="text" name="password" placeholder="password" style="height: 20px"><br><BR>--%>
+<%--    <input type="submit" value="Sign in">--%>
+<%--</form>--%>
 </body>
 </html>
 
